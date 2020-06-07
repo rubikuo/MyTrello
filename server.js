fce5ceb7e05e07821d3fe3dda1ff698c38443b89
Rubi Kuo <rubitiara@gmail.com>
2020-06-07 21:29:05 +0200

    commit

--- server.js
+++ server.js
@@ -99,8 +99,8 @@ app.use("/auth", authRoute);
 
 if(process.env.NODE_ENV === "production"){
   console.log("production!");
-  app.use(express.static(__dirname + "/trelloapp/build"));
-  app.get("/.*/", (req, res)=>{
+  app.use(express.static(path.join(__dirname + "/trelloapp/build")));
+  app.get("*", (req, res)=>{
     res.sendFile(path.join(__dirname, '/trelloapp/build', 'index.html'))
 });
 }
