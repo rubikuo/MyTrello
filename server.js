0f940697790428b676a0d155bce77f1813ae4da4
Rubi Kuo <rubitiara@gmail.com>
2020-06-07 22:19:06 +0200

    build

--- server.js
+++ server.js
@@ -99,17 +99,17 @@ app.use("/auth", authRoute);
 
 
 
-app.use(express.static(path.join(__dirname, '/trelloapp/build')));
+// app.use(express.static(path.join(__dirname, '/trelloapp/build')));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/trelloapp/public/index.html'))
-});
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, '/trelloapp/public/index.html'))
+// });
 
 if(process.env.NODE_ENV === "production"){
   console.log("production!");
-  app.use(express.static(path.join(__dirname + "/trelloapp/build")));
+  app.use(express.static(path.join(__dirname + "trelloapp/build")));
   app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, '/trelloapp/public/index.html'))
+    res.sendFile(path.join(__dirname = 'trelloapp/build/index.html'))
 });
 }
 
