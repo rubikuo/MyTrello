270168efa91bef957b262846390678a674cc32f6
Rubi Kuo <rubitiara@gmail.com>
2020-06-08 01:15:27 +0200

    __dirname + 'client/build/index.html'

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ if(process.env.NODE_ENV === "production"){
   console.log("production!");
   app.use(express.static(path.join(__dirname + "trelloapp/build")));
   app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname = 'trelloapp/build/index.html'));
+    res.sendFile(path.join(__dirname + 'trelloapp/build/index.html'));
 });
 }
 
