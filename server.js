21d5ab2b76dce61dd619d6aa5aa1fddcd6f759d3
Rubi Kuo <rubitiara@gmail.com>
2020-06-07 20:48:51 +0200

    path

--- server.js
+++ server.js
@@ -100,8 +100,8 @@ app.use("/auth", authRoute);
 if(process.env.NODE_ENV === "production"){
   console.log("production!");
   app.use(express.static("trelloapp/build"));
-  app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, 'trelloapp/build', 'index.html'))
+  app.get("/*", (req, res)=>{
+    res.sendFile(path.join(__dirname, '/trelloapp/build', 'index.html'))
 });
 }
 
