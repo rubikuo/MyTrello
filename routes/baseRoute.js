d2d1f8ac9eef8a509ced2c084ecc5ec046879d84
rubikuo <rubitiara@gmail.com>
2023-04-27 15:59:24 +0200

    updated

--- routes/baseRoute.js
+++ routes/baseRoute.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
- router.get("/", (req, res) => {
-    console.log(req.session);
-    if (req.session.viewCount) {
-      req.session.viewCount = req.session.viewCount + 1;
-    } else {
-      req.session.viewCount = 1;
-    }
-    res.send(
-      `<h1>You have visited this page ${req.session.viewCount} times </h1>`
-    );
-  });
+router.get("/", (req, res) => {
+  console.log(req.session);
+  if (req.session.viewCount) {
+    req.session.viewCount = req.session.viewCount + 1;
+  } else {
+    req.session.viewCount = 1;
+  }
+  res.send(
+    `<h1>You have visited this page ${req.session.viewCount} times </h1>`
+  );
+});
 
-  
 module.exports = router;
